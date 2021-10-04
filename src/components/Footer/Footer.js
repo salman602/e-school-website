1da3407f3080231a4849d3d08b951bcde13df4e8
salman602 <salmanrahman602@gmail.com>
2021-10-04 21:42:06 +0600

    some fine tuning

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,58 +11,60 @@ const Footer = () => {
     const phoneIcon = <FontAwesomeIcon icon={faPhoneAlt} />
     const emailIcon = <FontAwesomeIcon icon={faEnvelope} />
     return (
-        <Container className="my-3">
-            <Row>
-                <Col xs={6} md={4}>
-                    <h5 className="ms-3">E- School</h5>
-                    <div className="d-flex flex-column">
-                        <NavLink className="menu" to="/home" activeClassName="selected">
-                            Home
-                        </NavLink>
-                        <NavLink className="menu" to="/about" activeClassName="selected">
-                            About Us
-                        </NavLink>
-                        <NavLink className="menu" to="/courses" activeClassName="selected">
-                            Courses
-                        </NavLink>
-                        <NavLink className="menu" to="/blogs" activeClassName="selected">
-                            Blogs
-                        </NavLink>
-                        <NavLink className="menu" to="/contact" activeClassName="selected">
-                            Contact Us
-                        </NavLink>
-                    </div>
-                </Col>
-                <Col xs={6} md={4}>
-                    <h5 className="ms-3">Useful Links</h5>
-                    <div className="d-flex flex-column">
-                        <NavLink className="menu" to="/help" activeClassName="selected">
-                            Help Desk
-                        </NavLink>
-                        <NavLink className="menu" to="/facilities" activeClassName="selected">
-                            Facilities
-                        </NavLink>
-                        <NavLink className="menu" to="/support" activeClassName="selected">
-                            Student Support
-                        </NavLink>
-                        <NavLink className="menu" to="/policy" activeClassName="selected">
-                            Privacy & policy
-                        </NavLink>
-                        <NavLink className="menu" to="/terms" activeClassName="selected">
-                            Terms & Conditions
-                        </NavLink>
-                    </div>
-                </Col>
-                <Col xs={6} md={4}>
-                    <h5>Contact Info</h5>
-                    <h6>{mapIcon} 15, Hazi Tower, Satarkul Bridge, North Badda, Dhaka.</h6>
-                    <h6>{phoneIcon} +88 01234-567890</h6>
-                    <h6>{emailIcon} info@eschool.com</h6>
+        <div className="custom-bg">
+            <Container className="py-3">
+                <Row>
+                    <Col xs={6} md={4}>
+                        <h5 className="ms-3">E- School</h5>
+                        <div className="d-flex flex-column">
+                            <NavLink className="menu" to="/home" activeClassName="selected">
+                                Home
+                            </NavLink>
+                            <NavLink className="menu" to="/about" activeClassName="selected">
+                                About Us
+                            </NavLink>
+                            <NavLink className="menu" to="/courses" activeClassName="selected">
+                                Courses
+                            </NavLink>
+                            <NavLink className="menu" to="/blogs" activeClassName="selected">
+                                Blogs
+                            </NavLink>
+                            <NavLink className="menu" to="/contact" activeClassName="selected">
+                                Contact Us
+                            </NavLink>
+                        </div>
+                    </Col>
+                    <Col xs={6} md={4}>
+                        <h5 className="ms-3">Useful Links</h5>
+                        <div className="d-flex flex-column">
+                            <NavLink className="menu" to="/help" activeClassName="selected">
+                                Help Desk
+                            </NavLink>
+                            <NavLink className="menu" to="/facilities" activeClassName="selected">
+                                Facilities
+                            </NavLink>
+                            <NavLink className="menu" to="/support" activeClassName="selected">
+                                Student Support
+                            </NavLink>
+                            <NavLink className="menu" to="/policy" activeClassName="selected">
+                                Privacy & policy
+                            </NavLink>
+                            <NavLink className="menu" to="/terms" activeClassName="selected">
+                                Terms & Conditions
+                            </NavLink>
+                        </div>
+                    </Col>
+                    <Col xs={6} md={4}>
+                        <h5>Contact Info</h5>
+                        <h6>{mapIcon} 15, Hazi Tower, Satarkul Bridge, North Badda, Dhaka.</h6>
+                        <h6>{phoneIcon} +88 01234-567890</h6>
+                        <h6>{emailIcon} info@eschool.com</h6>
 
-                </Col>
-            </Row>
-            <p className="text-center mt-4">&copy; E-School 2022 All Rights reserved.</p>
-        </Container>
+                    </Col>
+                </Row>
+                <p className="text-center mt-4">&copy; E-School 2022 All Rights reserved.</p>
+            </Container>
+        </div>
     );
 };
 
